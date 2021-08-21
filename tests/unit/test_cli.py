2a4783f052de843dcce979d6da7d1c6f8de7a3fc
bengabay11 <ben.gabay38@gmail.com>
2021-08-21 15:03:50 +0300

    change unit tests to include the new cli flags

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,7 +1,7 @@
 import pytest
 
 from chpass.cli import parse_args
-from chpass.config import DEFAULT_EXPORT_DESTINATION_FOLDER, DEFAULT_FILE_ADAPTER
+from chpass.config import DEFAULT_FILE_ADAPTER
 
 
 @pytest.fixture(scope="module")
@@ -15,47 +15,13 @@ def import_mode() -> str:
 
 
 @pytest.fixture(scope="module")
-def from_file() -> str:
+def input_file_path() -> str:
     return "passwords.csv"
 
 
-def test_default_export(export_mode, connected_user):
-    args = parse_args([export_mode])
-    assert args.mode == export_mode
-    assert args.user == connected_user
-    assert args.destination_folder == DEFAULT_EXPORT_DESTINATION_FOLDER
-    assert args.file_adapter == DEFAULT_FILE_ADAPTER
-    assert not hasattr(args, "from_file")
-
-
-def test_default_import(import_mode, connected_user, from_file):
-    args = parse_args([import_mode, "-f", from_file])
-    assert args.mode == import_mode
-    assert args.user == connected_user
-    assert not hasattr(args, "destination_folder")
-    assert args.file_adapter == "csv"
-    assert args.from_file == from_file
-
-
-def test_user_flag_export(export_mode, connected_user):
-    user = connected_user
-    args = parse_args(["-u", user, export_mode])
-    assert args.mode == export_mode
-    assert args.user == user
-
-
-def test_user_flag_import(import_mode, from_file, connected_user):
-    user = connected_user
-    args = parse_args(["-u", user, import_mode, "-f", from_file])
-    assert args.mode == import_mode
-    assert args.user == user
-
-
-def test_file_adapter_flag_export(export_mode):
-    file_adapter = export_mode
-    args = parse_args(["-i", file_adapter, export_mode])
-    assert args.mode == export_mode
-    assert args.file_adapter == file_adapter
+@pytest.fixture(scope="module")
+def output_file() -> str:
+    return "import"
 
 
 @pytest.fixture
@@ -63,18 +29,65 @@ def correct_file_adapter() -> str:
     return "json"
 
 
-def test_file_adapter_flag_import(import_mode, correct_file_adapter, from_file):
-    args = parse_args(["-i", correct_file_adapter, import_mode, "-f", from_file])
+def test_default_export(export_mode, connected_user, output_file):
+    args = parse_args([export_mode, "-o", output_file])
+    assert args.mode == export_mode
+    assert args.user == connected_user
+    assert args.output_file == output_file
+    assert args.file_adapter == DEFAULT_FILE_ADAPTER
+    assert not hasattr(args, "file_path")
+
+
+def test_default_import(import_mode, connected_user, input_file_path):
+    args = parse_args([import_mode, "-i", input_file_path])
+    assert args.mode == import_mode
+    assert args.user == connected_user
+    assert not hasattr(args, "output_file")
+    assert args.file_adapter == DEFAULT_FILE_ADAPTER
+    assert args.input_file_path == input_file_path
+
+
+def test_user_flag_export(export_mode, connected_user, output_file):
+    user = connected_user
+    args = parse_args(["-u", user, export_mode, "-o", output_file])
+    assert args.mode == export_mode
+    assert args.user == user
+
+
+def test_mode_failed(connected_user):
+    user = connected_user
+    with pytest.raises(SystemExit):
+        parse_args(["-u", user])
+
+
+def test_export_output_flag_failed(export_mode, connected_user):
+    user = connected_user
+    with pytest.raises(SystemExit):
+        parse_args(["-u", user, export_mode])
+
+
+def test_import_input_flag_failed(import_mode, connected_user):
+    user = connected_user
+    with pytest.raises(SystemExit):
+        parse_args(["-u", user, import_mode])
+
+
+def test_user_flag_import(import_mode, connected_user, input_file_path):
+    user = connected_user
+    args = parse_args(["-u", user, import_mode, "-i", input_file_path])
+    assert args.mode == import_mode
+    assert args.user == user
+    assert args.input_file_path == input_file_path
+
+
+def test_file_adapter_flag_export(export_mode, correct_file_adapter, output_file):
+    args = parse_args(["-f", correct_file_adapter, export_mode, "-o", output_file])
+    assert args.mode == export_mode
+    assert args.file_adapter == correct_file_adapter
+
+
+def test_file_adapter_flag_import(import_mode, correct_file_adapter, input_file_path):
+    args = parse_args(["-f", correct_file_adapter, import_mode, "-i", input_file_path])
     assert args.mode == import_mode
     assert correct_file_adapter == correct_file_adapter
-
-
-@pytest.fixture
-def correct_destination_folder() -> str:
-    return "test"
-
-
-def test_export_destination_folder_flag(export_mode, correct_destination_folder):
-    args = parse_args([export_mode, "-d", correct_destination_folder])
-    assert args.mode == export_mode
-    assert args.destination_folder == correct_destination_folder
+    assert args.input_file_path == input_file_path
