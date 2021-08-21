066c2fc65d2207acc9a6b0c70cafe92cc42d1503
bengabay11 <ben.gabay38@gmail.com>
2021-08-21 15:17:11 +0300

    add type annotations to unit tests

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -29,7 +29,7 @@ def correct_file_adapter() -> str:
     return "json"
 
 
-def test_default_export(export_mode, connected_user, output_file_path):
+def test_default_export(export_mode: str, connected_user: str, output_file_path: str) -> None:
     args = parse_args([export_mode, "-o", output_file_path])
     assert args.mode == export_mode
     assert args.user == connected_user
@@ -38,7 +38,7 @@ def test_default_export(export_mode, connected_user, output_file_path):
     assert not hasattr(args, "file_path")
 
 
-def test_default_import(import_mode, connected_user, input_file_path):
+def test_default_import(import_mode: str, connected_user: str, input_file_path: str) -> None:
     args = parse_args([import_mode, "-i", input_file_path])
     assert args.mode == import_mode
     assert args.user == connected_user
@@ -47,32 +47,32 @@ def test_default_import(import_mode, connected_user, input_file_path):
     assert args.input_file_path == input_file_path
 
 
-def test_user_flag_export(export_mode, connected_user, output_file_path):
+def test_user_flag_export(export_mode: str, connected_user: str, output_file_path: str) -> None:
     user = connected_user
     args = parse_args(["-u", user, export_mode, "-o", output_file_path])
     assert args.mode == export_mode
     assert args.user == user
 
 
-def test_mode_failed(connected_user):
+def test_mode_failed(connected_user: str) -> None:
     user = connected_user
     with pytest.raises(SystemExit):
         parse_args(["-u", user])
 
 
-def test_export_output_flag_failed(export_mode, connected_user):
+def test_export_output_flag_failed(export_mode: str, connected_user: str) -> None:
     user = connected_user
     with pytest.raises(SystemExit):
         parse_args(["-u", user, export_mode])
 
 
-def test_import_input_flag_failed(import_mode, connected_user):
+def test_import_input_flag_failed(import_mode: str, connected_user: str) -> None:
     user = connected_user
     with pytest.raises(SystemExit):
         parse_args(["-u", user, import_mode])
 
 
-def test_user_flag_import(import_mode, connected_user, input_file_path):
+def test_user_flag_import(import_mode: str, connected_user: str, input_file_path: str) -> None:
     user = connected_user
     args = parse_args(["-u", user, import_mode, "-i", input_file_path])
     assert args.mode == import_mode
@@ -80,13 +80,13 @@ def test_user_flag_import(import_mode, connected_user, input_file_path):
     assert args.input_file_path == input_file_path
 
 
-def test_file_adapter_flag_export(export_mode, correct_file_adapter, output_file_path):
+def test_file_adapter_flag_export(export_mode: str, correct_file_adapter: str, output_file_path: str) -> None:
     args = parse_args(["-f", correct_file_adapter, export_mode, "-o", output_file_path])
     assert args.mode == export_mode
     assert args.file_adapter == correct_file_adapter
 
 
-def test_file_adapter_flag_import(import_mode, correct_file_adapter, input_file_path):
+def test_file_adapter_flag_import(import_mode: str, correct_file_adapter: str, input_file_path: str) -> None:
     args = parse_args(["-f", correct_file_adapter, import_mode, "-i", input_file_path])
     assert args.mode == import_mode
     assert correct_file_adapter == correct_file_adapter
