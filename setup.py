df274acc7ee6b8cce29c5885d3b08ee835048121
Ben Gabay <ben.gabay38@gmail.com>
2025-08-22 16:40:30 +0000

    upgrade version in setup.py from 0.3.1 to 0.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="chpass",
-    version="0.3.1",
+    version="0.3.4",
     author="Ben Gabay",
     author_email="ben.gabay38@gmail.com",
     license="License :: OSI Approved :: MIT License",
