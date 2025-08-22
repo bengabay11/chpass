d3a60fb6934d85c6f8ff635cbce522b2d58abbf0
Ben Gabay <ben.gabay38@gmail.com>
2025-08-22 21:02:09 +0000

    bump version to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="chpass",
-    version="0.3.6",
+    version="0.4.0",
     author="Ben Gabay",
     author_email="ben.gabay38@gmail.com",
     license="License :: OSI Approved :: MIT License",
